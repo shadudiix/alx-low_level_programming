8d8465cbbc7aef842920dd839b5b9003ef008802
shadudiix <shady.elmaadawy1998@gmail.com>
2023-09-07 19:00:13 -0700

    update puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main ' a program that print a line with puts function
-* Return: 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-(
-	puts("\Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
