04993d0a477fe1fe9ac675404ba837fb5adf47f9
shadudiix <shady.elmaadawy1998@gmail.com>
2023-09-14 19:24:30 -0700

    add main files

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,22 @@
+
+/*
+ * File: 1-isdigit.c
+ * Auth: Gideon Obae Gekonge
+ */
+
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
  */
 int _isdigit(int c)
 {
- return (c >= '0' && c <= '9');
-}
+	if (c >= '0' && c <= '9')
+		return (1);
 
+	else
+		return (0);
+}
